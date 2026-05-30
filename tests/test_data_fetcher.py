dc17d89f2defd9dfd2782ce10a17b81df68146e3
Seval Patel <seval.patel1997@gmail.com>
2026-05-30 18:27:43 +0530

    fix import issue in test

--- tests/test_data_fetcher.py
+++ tests/test_data_fetcher.py
@@ -1,7 +1,7 @@
 """Tests for data fetcher module - Indian Market."""
 
 import unittest
-from src.data_fetcher import DataFetcher
+from src.stocks.data_fetcher import DataFetcher
 
 
 class TestDataFetcher(unittest.TestCase):
