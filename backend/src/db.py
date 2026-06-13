65898604d99349217b9dc25170c1900b34647827
Seval Patel <seval.patel1997@gmail.com>
2026-06-13 17:37:43 +0530

    add user profile data insertion/updation api

--- backend/src/db.py
+++ backend/src/db.py
@@ -4,3 +4,4 @@ from pymongo import MongoClient
 client = MongoClient("mongodb://localhost:27017/")
 db = client["myappdb"]
 users_collection = db["users"]
+usersProfile_collection = db["userProfile"]
--- backend/src/users/signup.py
+++ backend/src/users/signup.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify, request, Blueprint
 from flask_cors import CORS
-from src.db import users_collection
+from src.db import users_collection, usersProfile_collection
 
 app = Flask(__name__)
 CORS(app)
@@ -17,4 +17,30 @@ def signup():
     users_collection.insert_one(user)
     return jsonify({"message": "User registered successfully"})
 
+@app.route("/api/users/addUserProfile", methods=["POST"])
+def addUserProfile():
+    data = request.json
+    user = {
+        "name": data.get("name"),
+        "mobile": data.get("mobile"),
+        "dob": data.get("dob"),
+        "username": data.get("username")
+    }
+    print(f"Update {data}")
 
+    # ✅ Call with argument
+    if userProfileExist(data.get("username")):
+        usersProfile_collection.update_one(
+            {"username": data.get("username")},
+            {"$set": user}
+        )
+        return jsonify({"message": "User profile updated successfully"}), 200
+    else:
+        print("user does not exist")
+        usersProfile_collection.insert_one(user)
+        return jsonify({"message": "User profile inserted successfully"}), 201
+
+
+def userProfileExist(username):
+    user = usersProfile_collection.find_one({"username": username})
+    return bool(user)
