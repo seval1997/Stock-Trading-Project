29b2620ec6e134aa6602a781c27aa51b684c6bba
Seval Patel <seval.patel1997@gmail.com>
2026-06-16 21:30:33 +0530

    add watchlist add and remove api

--- backend/app.py
+++ backend/app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_cors import CORS
 from src.stocks.dataFetch import fetch_nse_symbols, get_stock_data_by_symbol, nifty50_dashboard_card, nifty50_detailed_view
 from src.users.signup import signup, addUserProfile
+from src.stocks.stockWatchlist import addStockIntoWatchList, removeStockFromWatchList
 from src.users.login import login
 from src.users.users import userData
 
@@ -16,6 +17,8 @@ app.add_url_rule("/api/users/signup", view_func=signup, methods=["POST"])
 app.add_url_rule("/api/users/login", view_func=login, methods=["POST"])
 app.add_url_rule("/api/users/userData", view_func=userData)
 app.add_url_rule("/api/users/addUserProfile", view_func=addUserProfile, methods=["POST"])
+app.add_url_rule("/api/stocks/addStockWatchList", view_func=addStockIntoWatchList, methods=["POST"])
+app.add_url_rule("/api/stocks/removeStockWatchList", view_func=removeStockFromWatchList, methods=["POST"])
 
 if __name__ == "__main__":
     app.run(debug=True)
--- backend/src/db.py
+++ backend/src/db.py
@@ -3,5 +3,6 @@ from pymongo import MongoClient
 
 client = MongoClient("mongodb://localhost:27017/")
 db = client["myappdb"]
-users_collection = db["users"]
-usersProfile_collection = db["userProfile"]
+users_collection = db["user_accounts"]
+usersProfile_collection = db["user_profiles"]
+watchList_collection = db["stock_watchlists"]
